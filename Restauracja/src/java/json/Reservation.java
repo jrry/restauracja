ae0a9c8c1c048df5970fb399512c4dc7b3a7a1e0
Jarosław Pawłowski <jerry017@gmail.com>
2017-05-05 15:30:29 +0200

    Reservation update

--- Restauracja/src/java/json/Reservation.java
+++ Restauracja/src/java/json/Reservation.java
@@ -17,6 +17,53 @@ public class Reservation {
         this.minutes = minutes;
         this.guests = guests;
     }
-    
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(Integer minutes) {
+        this.minutes = minutes;
+    }
+
+    public Integer getGuests() {
+        return guests;
+    }
+
+    public void setGuests(Integer guests) {
+        this.guests = guests;
+    }
     
 }
