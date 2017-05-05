d81b6cd479a8e5f447bf76dade2f8e9eae7b05ad
Jarosław Pawłowski <jerry017@gmail.com>
2017-05-05 12:45:32 +0200

    2

---
+++ Restauracja/src/java/reservation/MainReservation.java
@@ -0,0 +1,26 @@
+
+package reservation;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainReservation implements Serializable {
+    
+    private Person person;
+
+    public MainReservation() {
+        person = new Person();
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+    
+}
---
+++ Restauracja/src/java/reservation/Person.java
@@ -0,0 +1,42 @@
+package reservation;
+
+
+public class Person {
+    private String name;
+    private String surname;
+    private String mail;
+    private Integer phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+    
+}
